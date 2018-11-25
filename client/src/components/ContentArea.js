be2d024b81a88680afe9009bb0a0881a7c3c8dd6
John Ahn <john@launchpartner.co>
2018-11-25 02:27:45 -0800

    first commit

---
+++ client/src/components/ContentArea.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Home from './Home';
+
+class ContentArea extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+        //declare methods here
+
+    }
+
+    componentDidMount() {
+        // onInit
+    }
+
+    render() {
+
+        return(
+            <React.Fragment>
+                <div className="root-div">
+
+                    <Home/>
+
+                </div>
+            </React.Fragment>
+        );
+    }
+
+
+
+}
+
+
+export default ContentArea;
---
+++ client/src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+        // declare methods here
+    }
+
+    componentDidMount() { }
+
+    render() {
+        return(<React.Fragment>
+
+            <div className="">
+
+
+
+            </div>
+
+        </React.Fragment>);
+    }
+
+}
+
+export default Home;
